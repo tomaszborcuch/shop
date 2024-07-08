759f16ed6ba1afb39a5db9b4c93f4058f6cff6c5
Tomasz <tomas.borcuch@gmail.com>
2024-07-08 22:30:07 +0200

    add new variables and new asertions

--- e2e/cypress/e2e/shop.cy.js
+++ e2e/cypress/e2e/shop.cy.js
@@ -1,29 +1,49 @@
-import {uniqueuser} from './userdata.cy'
+import { uniqueuser } from './userdata.cy'
+import { userselectors } from './userselectors.cy'
+
 describe('Shop with electronic devices', () => {
-    it('Registration test', () => {
+    
+    it('Test rejestracji', () => {
         const user = uniqueuser()
-        cy.visit('https://www.sferis.pl/rejestracja')
-        cy.get('#cookiescript_accept').then(($button) => {
-            if($button.is(':visible')){
-                ($button).click()
+        const sel = userselectors()
+    
+        cy.visit('https://botland.com.pl/logowanie?create_account=1')
+        //Sprawdzmy czy jesteśmy na panelu stworzenia konta
+        cy.contains('Stwórz konto').should('exist')
+    
+        cy.get(sel.cookiesButton).then(($button) => {
+            if ($button.is(':visible')) {
+                cy.wrap($button).click()
             }
         })
-        // Sprawdzenie, czy wszystkie pola są widoczne i puste
-        cy.get('#signuplogin').should('be.visible').and('be.empty')
-        cy.get('#signuppassword').should('be.visible').and('be.empty')
-        cy.xpath('#signuppasswordrepeat').should('be.visible').and('be.empty')
-        cy.xpath('label[for="consent01"]').should('be.empty')
+    
+        // Sprawdzenie, czy pola są widoczne i puste
+        cy.get(sel.firstname).should('be.visible').and('be.empty')
+        cy.get(sel.lastname).should('be.visible').and('be.empty')
+        cy.get(sel.email).should('be.visible').and('be.empty')
+        cy.get(sel.password).should('be.visible').and('be.empty')
+        cy.get(sel.checkboxowu).should('be.visible')
+        cy.get(sel.checkboxrulez).should('be.visible')
+        cy.get(sel.submit).should('be.visible')
+
+    
         // Wprowadzenie wartości do pól
-        cy.get('input[name="firstname"]').type(user.username)
-        cy.get('input[name="lastname"]').type(user.lastname)
-        cy.get('input[name="email"]').type(user.email)
-        cy.get('input[name="password"]').type(user.password)
+        cy.get(sel.firstname).type(user.username)
+        cy.get(sel.lastname).type(user.lastname)
+        cy.get(sel.email).type(user.email)
+        cy.get(sel.password).type(user.password)
+        cy.get(sel.checkboxowu).check()
+        cy.get(sel.checkboxrulez).check()
+    
         // Sprawdzenie, czy wartości zostały wprowadzone poprawnie
-        cy.get('input[name="firstname"]').should('have.value', user.username)
-        cy.get('input[name="lastname"]').should('have.value', user.lastname)
-        cy.get('input[name="email"]').should('have.value', user.email)
-        cy.get('input[name="password"]').should('have.value', user.password)
-        cy.get('input[name="addictional-user-terms-checkbox"]').click()
-        cy.get('input[name="psgdpr"]').click()
+        cy.get(sel.firstname).should('have.value', user.username)
+        cy.get(sel.lastname).should('have.value', user.lastname)
+        cy.get(sel.email).should('have.value', user.email)
+        cy.get(sel.password).should('have.value', user.password)
+        cy.get(sel.checkboxowu).should('be.checked')
+        cy.get(sel.checkboxrulez).should('be.checked')
+        // Sprawdzenie, czy email zawiera znak @ 
+        cy.get(sel.email).invoke('val').should('include', '@')
     })
 })
+    
---
+++ e2e/cypress/e2e/userselectors.cy.js
@@ -0,0 +1,12 @@
+export function userselectors() {
+    return {
+        cookiesButton: "#cookiescript_accept",
+        firstname: "input[name='firstname']",
+        lastname: "input[name='lastname']",
+        email: "input[name='email']",
+        password: "input[name='password']",
+        checkboxowu: "label[for='f-addictional-user-terms-checkbox_1']",
+        checkboxrulez: "label[for='f-psgdpr_18594']",
+        submit: "//button[normalize-space()='Stwórz konto']"
+    }
+}
